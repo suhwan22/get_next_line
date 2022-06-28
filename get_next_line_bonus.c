de70b33a0ab2b7cfd0340e6e373772b5ddf94795
Suhwan Kim <suhkim@c1r6s5.42seoul.kr>
2022-06-28 20:15:59 +0900

    b first

--- get_next_line_bonus.c
+++ get_next_line_bonus.c
@@ -6,28 +6,11 @@
 /*   By: suhkim <suhkim@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/20 14:38:11 by suhkim            #+#    #+#             */
-/*   Updated: 2022/06/22 18:56:04 by suhkim           ###   ########.fr       */
+/*   Updated: 2022/06/28 18:11:07 by suhkim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "get_next_line.h"
-
-#define BUFFER_SIZE 10
-
-t_list	*add_front(t_list *head, int fd)
-{
-	t_list	*new;
-
-	new = (t_list *)malloc(sizeof(t_list));
-	if (!new)
-		return (0);
-	new->fd = fd;
-	new->next = head->next;
-	new->pre = head;
-	new->str = 0;
-	head->next = new;
-	return (new);
-}
+#include "get_next_line_bonus.h"
 
 t_list	*check_fd(t_list *head, int fd)
 {
@@ -37,12 +20,20 @@ t_list	*check_fd(t_list *head, int fd)
 	while (temp)
 	{
 		if (fd == temp->fd)
-		{
 			return (temp);
-		}
 		temp = temp->next;
 	}
-	return (add_front(head, fd));
+	temp = (t_list *)malloc(sizeof(t_list));
+	if (!temp)
+		return (0);
+	temp->fd = fd;
+	temp->next = head->next;
+	temp->pre = head;
+	temp->str = 0;
+	if (head->next)
+		head->next->pre = temp;
+	head->next = temp;
+	return (temp);
 }
 
 char	*read_fd(t_list *node)
@@ -52,7 +43,11 @@ char	*read_fd(t_list *node)
 
 	buf = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
 	if (!buf)
+	{
+		if (node->str)
+			free(node->str);
 		return (0);
+	}
 	size = 1;
 	while (size > 0 && !ft_strchr(node->str, '\n'))
 	{
@@ -70,33 +65,41 @@ char	*read_fd(t_list *node)
 	return (node->str);
 }
 
-char	*get_one_line(t_list *node)
+char	*get_one_line(char	*str)
 {
 	char	*ans;
-	char	*temp;
-	size_t	len;
-	size_t	end;
+	char	*ptr;
 
-	len = 0;
-	while (*(node->str + len) && *(node->str + len) != '\n')
-		len++;
-	ans = (char *)malloc(sizeof(char) * (len + 1));
+	if (!str)
+		return (0);
+	if (*(str) == 0)
+		return (0);
+	ptr = ft_strchr(str, '\n');
+	if (!ptr)
+	{
+		return (ft_strdup(str));
+	}
+	ans = (char *)malloc(sizeof(char) * (ptr - str + 2));
 	if (!ans)
 		return (0);
-	ft_strlcpy(ans, node->str, len + 1);
-	end = 0;
-	while (*(node->str + end))
-		end++;
-	if (end - len)
-	{
-		temp = (char *)malloc(sizeof(char) * (end - len));
-		if (!temp)
-			return (0);
-		ft_strlcpy(temp, node->str + len + 1, end - len);
-		free(node->str);
-		node->str = temp;
-	}
+	ft_strlcpy(ans, str, ptr - str + 2);
 	return (ans);
+}
+
+char	*remain_line(t_list *node)
+{
+	char	*ptr;
+	char	*remain;
+
+	ptr = ft_strchr(node->str, '\n');
+	if (!ptr)
+	{
+		free(node->str);
+		return (0);
+	}
+	remain = ft_strdup(ptr + 1);
+	free(node->str);
+	return (remain);
 }
 
 char	*get_next_line(int fd)
@@ -105,17 +108,20 @@ char	*get_next_line(int fd)
 	t_list			*node;
 	char			*ans;
 
+	if (fd < 0 || BUFFER_SIZE < 1)
+		return (0);
 	node = check_fd(&head, fd);
 	if (!node)
-	{
-		lst_del(&head);
 		return (0);
-	}
 	if (!read_fd(node))
 	{
-		lst_del(&head);
+		if (!node->str)
+			lst_del(node);
 		return (0);
 	}
-	ans = get_one_line(node);
+	ans = get_one_line(node->str);
+	node->str = remain_line(node);
+	if (!node->str)
+		lst_del(node);
 	return (ans);
 }
